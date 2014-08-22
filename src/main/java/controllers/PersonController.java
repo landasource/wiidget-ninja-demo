7b5668896b11a12f4c6474128f26db99120fcefd
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2014-08-22 20:49:01 +0200

    Add UTF-8 encoding support

--- src/main/java/controllers/PersonController.java
+++ src/main/java/controllers/PersonController.java
@@ -17,7 +17,6 @@
 package controllers;
 
 import models.Person;
-import ninja.Context;
 import ninja.Result;
 import ninja.Results;
 
@@ -26,20 +25,20 @@ import com.google.inject.Singleton;
 @Singleton
 public class PersonController {
 
-    public Result getPerson() {
+	public Result getPerson() {
 
-        // simply render a json as result:
-        Person person = new Person();
-        person.name = "zeeess name - and some utf8 => öäü";
+		// simply render a json as result:
+		final Person person = new Person();
+		person.name = "zeeess name - and some utf8";
 
-        // render
-        return Results.json().render(person);
-    }
+		// render
+		return Results.json().render(person);
+	}
 
-    public Result postPerson(Person person) {
-        // okay... we simply render the parsed object again as json
-        // usually we would save something into a db or so..
-        return Results.json().render(person);
-    }
+	public Result postPerson(final Person person) {
+		// okay... we simply render the parsed object again as json
+		// usually we would save something into a db or so..
+		return Results.json().render(person);
+	}
 
 }
